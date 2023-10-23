dd99c38899c3fbd10b3834ea22673482b7b515ca
Florence1234567 <119359132+Florence1234567@users.noreply.github.com>
2023-10-23 09:10:34 -0400

    .

--- models/CollectionFilter.js
+++ models/CollectionFilter.js
@@ -7,25 +7,4 @@ export default class CollectionFilter extends Model {
         this.model = model;
         console.log(data, params, model);
     }
-
-    valueMatch(value, searchValue) {
-        try {
-            //let exp = '^' + searchValue.toLowerCase();.replace(/\*/g, '.*') + '$';
-            return new RegExp(exp).test(value.toString().toLowerCase());
-        } catch (error) {
-            console.log(error);
-            return false;
-        }
-    }
-    compareNum(x, y) {
-        if (x === y) return 0;
-        else if (x < y) return -1;
-        return 1;
-    }
-    innerCompare(x, y) {
-        if ((typeof x) === 'string')
-            return x.localeCompare(y);
-        else
-            return this.compareNum(x, y);
-    }
 }
